6ed84000002f9943524a624ccb2c18171071eb40
Ilyeo <jrag.tkd@gmail.com>
2018-11-24 21:50:49 -0600

    Add `ItemDecorator` class
    This class delegates the `update` method to the item instance.

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,47 +1,32 @@
-class GildedRose
+class ItemDecorator < SimpleDelegator
+  def update
+    item = self
 
-  def initialize(items)
-    @items = items
-  end
-
-  def update_quality()
-    @items.each do |item|
-      update(item)
-    end
-  end
-
-  def update(item)
-    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+    if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
       if item.quality > 0
-        if item.name != "Sulfuras, Hand of Ragnaros"
+        if item.name != 'Sulfuras, Hand of Ragnaros'
           item.quality = item.quality - 1
         end
       end
     else
       if item.quality < 50
         item.quality = item.quality + 1
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
           if item.sell_in < 11
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
+            item.quality = item.quality + 1 if item.quality < 50
           end
           if item.sell_in < 6
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
+            item.quality = item.quality + 1 if item.quality < 50
           end
         end
       end
     end
-    if item.name != "Sulfuras, Hand of Ragnaros"
-      item.sell_in = item.sell_in - 1
-    end
+    item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
     if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.name != 'Aged Brie'
+        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
           if item.quality > 0
-            if item.name != "Sulfuras, Hand of Ragnaros"
+            if item.name != 'Sulfuras, Hand of Ragnaros'
               item.quality = item.quality - 1
             end
           end
@@ -49,11 +34,25 @@ class GildedRose
           item.quality = item.quality - item.quality
         end
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
+        item.quality = item.quality + 1 if item.quality < 50
       end
     end
+  end
+end
+
+class GildedRose
+  def initialize(items)
+    @items = items
+  end
+
+  def update_quality
+    @items.each do |item|
+      update(ItemDecorator.new(item))
+    end
+  end
+
+  def update(item)
+    item.update
   end
 end
 
@@ -66,7 +65,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
