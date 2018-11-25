3eb0e08e3bf095553e6e49b7d8138f8d55af4e89
Ilyeo <jrag.tkd@gmail.com>
2018-11-24 22:03:36 -0600

    Remove `item` variable
    Use only `self` when we need to alter the attributes of the item.

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,40 +1,38 @@
 class ItemDecorator < SimpleDelegator
   def update
-    item = self
-
-    if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality = item.quality - 1
+    if (name != 'Aged Brie') && (name != 'Backstage passes to a TAFKAL80ETC concert')
+      if quality > 0
+        if name != 'Sulfuras, Hand of Ragnaros'
+          self.quality = self.quality - 1
         end
       end
     else
-      if item.quality < 50
-        item.quality = item.quality + 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            item.quality = item.quality + 1 if item.quality < 50
+      if quality < 50
+        self.quality = self.quality + 1
+        if name == 'Backstage passes to a TAFKAL80ETC concert'
+          if sell_in < 11
+            self.quality = self.quality + 1 if quality < 50
           end
-          if item.sell_in < 6
-            item.quality = item.quality + 1 if item.quality < 50
+          if sell_in < 6
+            self.quality = self.quality + 1 if quality < 50
           end
         end
       end
     end
-    item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
-    if item.sell_in < 0
-      if item.name != 'Aged Brie'
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality = item.quality - 1
+    self.sell_in = self.sell_in - 1 if name != 'Sulfuras, Hand of Ragnaros'
+    if sell_in < 0
+      if name != 'Aged Brie'
+        if name != 'Backstage passes to a TAFKAL80ETC concert'
+          if quality > 0
+            if name != 'Sulfuras, Hand of Ragnaros'
+              self.quality = self.quality - 1
             end
           end
         else
-          item.quality = item.quality - item.quality
+          self.quality = self.quality - self.quality
         end
       else
-        item.quality = item.quality + 1 if item.quality < 50
+        self.quality = self.quality + 1 if quality < 50
       end
     end
   end
